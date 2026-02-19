e76ae979b1ed8fc3ae82f7fd8ddf85d7b40bbe2d
otidaphilip <otida.anselmo@gmail.com>
2026-02-19 11:03:18 +0800

    fixed .env and db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -3,13 +3,13 @@ const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: true,
-  },
+  ssl: process.env.DB_SSL === "true"
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 module.exports = pool;
